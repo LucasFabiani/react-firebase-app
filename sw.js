6e94125c79fe97665cbf9274d55ef5dfe4d22b8d
LucasFabiani <lucasefabiani@gmail.com>
2025-02-12 16:07:57 -0300

    Probando instalar en android

--- sw.js
+++ sw.js
@@ -1,12 +1,22 @@
-self.addEventListener("install", (event) => {
-    console.log("Service Worker instalado");
-    event.waitUntil(self.skipWaiting());
-  });
-  
-  self.addEventListener("activate", (event) => {
-    console.log("Service Worker activado");
-  });
-  
-  self.addEventListener("fetch", (event) => {
-    console.log("Interceptando solicitud a:", event.request.url);
-  });
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+      caches.open('v1').then((cache) => {
+          return cache.addAll([
+              '/',
+              '/index.html',
+              '/styles.css',
+              '/script.js',
+              '/images/icon-192x192.png',
+              '/images/icon-512x512.png'
+          ]);
+      })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+      caches.match(event.request).then((response) => {
+          return response || fetch(event.request);
+      })
+  );
+});
